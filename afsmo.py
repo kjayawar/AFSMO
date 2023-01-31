a21c5e4bb02c3b1ec4115fdaef1efad6bab609e9
kjayawar <77035452+kjayawar@users.noreply.github.com>
2023-01-31 17:33:02 +1100

    Fixed interpolation issue
    1. Curvature discontinuity at the LE as a result of manual 0 inclusion of theta had been fixed.
    2. Theta distribution now goes from 180 to -180 creating counterclockwise points array
    3. Allows users to use either internal or external interpolation scheme. 
    4. Removed consecutive duplicate points introduced by AFSMO when TE is not properly ended with 1,0

--- afsmo.py
+++ afsmo.py
@@ -27,6 +27,18 @@ def clear_dir():
 		if os.path.exists(file):
 		    os.remove(file)
 
+def extract_afsmo_dat(n_inter, filename="afsmo.dat"):
+	"""
+	Reads the xy dump file created by AFSMO and spits out
+	the coordinates in standard *.dat format
+	"""
+	x, y= np.loadtxt(filename).T
+	x_upper = x[:n_inter][::-1]
+	y_upper = y[:n_inter][::-1]
+	x_lower = x[n_inter:][1:]
+	y_lower = y[n_inter:][1:]
+	return np.concatenate([x_upper,x_lower]), np.concatenate([y_upper,y_lower])
+
 def extract_datfile_title(filename):
 	"""
 	Reads title and x,y data from a standard *.dat file 
@@ -40,7 +52,7 @@ def extract_datfile_data(filename):
 	"""
 	return np.loadtxt(filename, skiprows=1)
 
-def gen_input(title, x,y, input_filename):
+def gen_input(title, x,y, input_filename, n_inter):
 	"""
 	Creates an input file for AFSMO. 
 	Standard *dat file x,y array is split from the LE to create 4 arrays on Upper and Lower XY data.
@@ -61,24 +73,21 @@ def gen_input(title, x,y, input_filename):
 	xy_upper = "\n".join("{:12.6f}{:12.6f}".format(x,y) for x,y in zip(xu,yu))
 	xy_lower = "\n".join("{:12.6f}{:12.6f}".format(x,y) for x,y in zip(xl,yl))
 
-	# Bug fix - 29-Jan-2023
-	# Problem 
-	# =======
-	# AFSMO.f tend to produce a duplicate point when interpolation option is used.
-	# Solution
-	# ========
-	# The new point generation through interpolation is now done through python
-	# so the user given n_inter is ignored here and fixed number is used.
-	# n_inter will be used later for interpolation
-
-	n_inter = 40
-
 	x_inter = 0.5*(1 + np.cos(np.linspace(np.pi, 0, n_inter)))
 	x_inter = "\n".join(["{:12.6f}".format(x) for x in x_inter])
 
 	file_content = template.format(title, nu, xy_upper, nl, xy_lower, n_inter, x_inter)
 	with open(input_filename, "w") as f:
 		f.write(file_content)
+
+def remove_consecutive_duplicates(x,y):
+	"""
+	Removes duplicate points by element-wise comparison between
+	the given array and shifted array. 
+	Indexing is done on	x-values [:,0]
+	"""
+	points = np.vstack((x,y)).T
+	return points[(points != np.roll(points,1,0))[:,0]].T
 
 def write2dat(title, x_new, y_new, filename):
 	"""
@@ -99,15 +108,27 @@ def extract_afsmo_smr(filename="afsmo.smr"):
 	return theta, x, y_smoo
 
 def interpolate_new_points(theta, x, y, n_inter):
+	# Bug fix - 29-Jan-2023
+	# Problem 
+	# =======
+	# AFSMO.f tend to produce a duplicate point when interpolation option is used.
+	# Solution
+	# ========
+	# The new point generation through interpolation is now done through python
+
 	theta_x_spline = interp1d(theta,x, kind='cubic', fill_value='extrapolate')
 	theta_y_spline = interp1d(theta,y, kind='cubic', fill_value='extrapolate')
 
-	# Make a new theta array to interpolate x and y on. 
-	# assumed its a good idea to include 0 to make sure LE is correctly placed on.
-	# https://stackoverflow.com/a/67480767/10737147
+	# To resemble n_inter as closely as possible with how AFSMO
+	# treats n_inter --n*x_values on one surface so that the total airfoil
+	# will have 2*n - 1 points after removing duplicate LE
+	# 180 to -180 such that the points are laid out counter-clockwise
 
-	new_theta = np.linspace(-180,180, n_inter)
-	new_theta = np.concatenate((new_theta[new_theta<0], [0], new_theta[new_theta>0]))
+	new_theta = np.linspace(180, -180, (n_inter*2)-1 )
+
+	# new_theta = np.concatenate((new_theta[new_theta<0], [0], new_theta[new_theta>0]))
+	# Adding 0 above is a bad idea. MSS proved the curvature discontinuity through Rhino
+	# Keeping this commented out in case I forget in the future.
 
 	new_x = theta_x_spline(new_theta)
 	new_y = theta_y_spline(new_theta)
@@ -127,7 +148,7 @@ def compare_plot(x,y, x_new, y_new, title, fix_aspect=False):
 		plt.gca().set_aspect("equal", 'datalim')
 	plt.show()
 
-def afsmo(dat_file, interpolate, n_inter, plotter, aspect, scale):
+def afsmo(dat_file, interpolate, n_inter, plotter, aspect, scale, external):
 	"""
 	main program
 	1. extracts data from standard dat file
@@ -139,6 +160,13 @@ def afsmo(dat_file, interpolate, n_inter, plotter, aspect, scale):
 	7. Plots both airfoils for comparison
 	"""
 	clear_dir()
+
+	# two interpolation schemes are supported on this program, internal and external
+	# when internal interpolation is used (AFSMO built-in interpolation)
+	# the maximum n_inter AFSMO supports is 100. 
+
+	afsmo_n_inter = min(n_inter, 100)
+
 	dat_basefilename = dat_file[:-4]
 	in_file = "{}.in".format(dat_basefilename)
 	out_file = dat_basefilename + "_sm.dat"
@@ -150,18 +178,24 @@ def afsmo(dat_file, interpolate, n_inter, plotter, aspect, scale):
 	else:
 		x, y  = extract_datfile_data(dat_file).T
 	
-	gen_input(title, x,y, in_file)
+	gen_input(title, x,y, in_file, afsmo_n_inter)
 	os.system("{} {}".format("afsmo.exe" if os.name == "nt" else "./afsmo", in_file))
 	
 	try:
-		theta, x_new, y_new = extract_afsmo_smr()
+		theta, x_new, y_new  = extract_afsmo_smr()
 		if interpolate:
-			x_new, y_new = interpolate_new_points(theta, x_new, y_new, n_inter)
+			if external:
+				x_new, y_new = interpolate_new_points(theta, x_new, y_new, n_inter)
+			else:
+				x_new, y_new = extract_afsmo_dat(afsmo_n_inter)
+				x_new, y_new = remove_consecutive_duplicates(x_new, y_new)
+				
 	except:
 		print("Failure encountered")
 		return
 
 	write2dat(title, x_new, y_new, out_file)
+
 	if plotter:
 		compare_plot(x,y, x_new, y_new, title, aspect)
 
@@ -193,5 +227,15 @@ if __name__ == '__main__':
 						help='Scales and de-rotates the airfoil [Normalize]', 
 						action='store_true')
 
+	parser.add_argument('-e', '--external',     
+						help='Use external interpolation instead of AFSMO built-in interpolation', 
+						action='store_true')
+
 	args = parser.parse_args()
-	afsmo(args.dat_file, args.interpolate, args.n_inter, args.plotter, args.aspect, args.scale)
+	afsmo(args.dat_file,
+		  args.interpolate, 
+		  args.n_inter,
+		  args.plotter, 
+		  args.aspect, 
+		  args.scale, 
+		  args.external)
